e94448aef2d6c5ee066b5c0c3232acc34c352686
Pooja298 <48399530+Pooja298@users.noreply.github.com>
2019-04-22 16:27:24 +0530

    Create sign-up.component.ts

---
+++ Client/src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+export class SignUpComponent implements OnInit {
+
+  constructor(private router: Router){ 
+  };
+
+  ngOnInit() {
+  }
+  submit(){
+    this.router.navigate(['/sector']);
+  }
+  cancel(){
+    this.router.navigate(['/home']);
+  }
+  
+
+}
